4a1ed3f12e196a8bfce3e7b752632807734c121e
Jelena Budakovic <jelenab@Jelenas-MBP.mobile.usilu.net>
2018-12-14 19:08:12 +0100

    schifo 2.0

--- grade_my_team/truffle.js
+++ grade_my_team/truffle.js
@@ -4,8 +4,10 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 9545,
-      network_id: "*" // Match any network id
+      port: 8545,
+      network_id: "*",// Match any network id
+        gas: 4700000,
+        gasPrice: 1000000000
     }
   }
 };
